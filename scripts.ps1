fde6ab0df6c1bb1eba0a3d3f9e8d6550129eeb18
rajeshjanapati <67915938+rajeshjanapati@users.noreply.github.com>
2023-09-07 10:04:34 +0530

    Create scripts.ps1

---
+++ scripts.ps1
@@ -0,0 +1,78 @@
+# write-output Apigee Artifacts
+$token = $env:TOKEN
+$org = $env:ORG
+$baseURL = "https://apigee.googleapis.com/v1/organizations/"
+$headers = @{Authorization = "Bearer $token"}
+
+# Set your GitHub repository information
+$repositoryOwner = "rajeshjanapati@gmail.com"
+$repositoryName = "apigee-encrypt-keys"
+$branchName = "main"  # Change this to the branch you want to access
+$githubToken = "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"
+
+# Import the cryptographic module
+Import-Module Microsoft.PowerShell.Security
+
+# # Define the encryption key and initialization vector (IV)
+# $encryptionKey = [System.Text.Encoding]::UTF8.GetBytes("YourEncryptionKey")
+# $iv = [System.Text.Encoding]::UTF8.GetBytes("YourInitializationVector")
+
+$encryptionKey = New-Object byte[] 32  # 256 bits
+$rng = [System.Security.Cryptography.RNGCryptoServiceProvider]::new()
+$rng.GetBytes($encryptionKey)
+
+$iv = New-Object byte[] 16  # 128 bits
+$rng = [System.Security.Cryptography.RNGCryptoServiceProvider]::new()
+$rng.GetBytes($iv)
+
+$appdetailget = Invoke-RestMethod -Uri "https://apigee.googleapis.com/v1/organizations/esi-apigee-x-394004/environments/eval/keyvaluemaps" -Method 'GET' -Headers $headers
+$appdetailjson = $appdetailget | ConvertTo-Json
+Write-Host $appdetailjson
+
+# # Define the JSON data
+# $jsonData = '{
+#     "name": "John Doe",
+#     "email": "johndoe@example.com",
+#     "phone": "555-123-4567"
+# }'
+# $jsonObject = ConvertFrom-Json $jsonData
+
+# # Define an array of field names to encrypt
+# $fieldsToEncrypt = @("email", "phone")
+
+# # Create an AES encryption object
+# $aes = New-Object System.Security.Cryptography.AesCryptoServiceProvider
+# $aes.Key = $encryptionKey
+# $aes.IV = $iv
+
+# # Create a crypto stream for encryption
+# $encryptor = $aes.CreateEncryptor()
+
+# # Loop through the JSON object and encrypt specified fields
+# foreach ($field in $fieldsToEncrypt) {
+#     if ($jsonObject.$field) {
+#         $valueToEncrypt = $jsonObject.$field
+
+#         # Convert the string to bytes
+#         $bytesToEncrypt = [System.Text.Encoding]::UTF8.GetBytes($valueToEncrypt)
+
+#         # Encrypt the bytes
+#         $encryptedBytes = $encryptor.TransformFinalBlock($bytesToEncrypt, 0, $bytesToEncrypt.Length)
+
+#         # Convert the encrypted bytes to Base64 or any other format as needed
+#         $encryptedValue = [System.Convert]::ToBase64String($encryptedBytes)
+
+#         # Replace the original value with the encrypted value
+#         $jsonObject.$field = $encryptedValue
+#     }
+# }
+
+# # Convert the modified JSON object back to a JSON string
+# $encryptedJsonData = $jsonObject | ConvertTo-Json
+
+# # Write the encrypted JSON data back to the same JSON file
+# $encryptedJsonData | Set-Content -Path "your_output.json" -Force
+
+# # Output the encrypted JSON data
+# Write-Host "Encrypted JSON Data:"
+# Write-Host $encryptedJsonData
