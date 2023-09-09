b95a88ef723c4cde4a6d1362f0ecb7d0e3a70e0b
rajeshjanapati <67915938+rajeshjanapati@users.noreply.github.com>
2023-09-09 07:31:52 +0530

    Update scripts.ps1

--- scripts.ps1
+++ scripts.ps1
@@ -4,18 +4,14 @@ $org = $env:ORG
 $baseURL = "https://apigee.googleapis.com/v1/organizations/"
 $headers = @{Authorization = "Bearer $token"}
 
-# Set your GitHub repository information
-$repositoryOwner = "rajeshjanapati@gmail.com"
-$repositoryName = "apigee-encrypt-keys"
-$branchName = "main"  # Change this to the branch you want to access
-$githubToken = "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"
+# # Set your GitHub repository information
+# $repositoryOwner = "rajeshjanapati@gmail.com"
+# $repositoryName = "apigee-encrypt-keys"
+# $branchName = "main"  # Change this to the branch you want to access
+# $githubToken = "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"
 
-# Import the cryptographic module
-Import-Module Microsoft.PowerShell.Security
-
-# # Define the encryption key and initialization vector (IV)
-# $encryptionKey = [System.Text.Encoding]::UTF8.GetBytes("YourEncryptionKey")
-# $iv = [System.Text.Encoding]::UTF8.GetBytes("YourInitializationVector")
+# # Import the cryptographic module
+# Import-Module Microsoft.PowerShell.Security
 
 $encryptionKey = New-Object byte[] 32  # 256 bits
 $rng = [System.Security.Cryptography.RNGCryptoServiceProvider]::new()
@@ -47,50 +43,6 @@ foreach ($field in $fieldsToEncode) {
     Encode-PropertyValue -object $appdetailget.credentials[0] -property $field
 }
 
-
-
 # Save the modified JSON data to a JSON file
 Write-host "Encrypted data: ($appdetailget | ConvertTo-Json)"
 $appdetailget | ConvertTo-Json | Set-Content -Path "jsonfiles/base64_encoded_app.json"
-
-# Clone the repository
-# git clone https://github.com/rajeshjanapati/apigee-encrypt-keys.git
-# cd apigee-encrypt-keys
-# git config --global user.email "rajeshjanapati@gmail.com"
-# git config --global user.name "rajeshjanapati"
-# git add apigee-encrypt-keys/jsonfiles/base64_encoded_app.json
-# git commit -m "Update encrypted JSON data"
-# git push -f origin main
-
-
-
-# # # Create a crypto stream for encryption
-# $encryptor = $aes.CreateEncryptor()
-
-# # Loop through the JSON object and encrypt specified fields
-# foreach ($field in $fieldsToEncrypt) {
-#     Write-Host "Entered into FOREACH..."
-#     $valueToEncrypt = $field
-
-#     # Convert the string to bytes
-#     $bytesToEncrypt = [System.Text.Encoding]::UTF8.GetBytes($valueToEncrypt)
-
-#     # Encrypt the bytes
-#     $encryptedBytes = $encryptor.TransformFinalBlock($bytesToEncrypt, 0, $bytesToEncrypt.Length)
-
-#     # Convert the encrypted bytes to Base64 or any other format as needed
-#     $encryptedValue = [System.Convert]::ToBase64String($encryptedBytes)
-
-#     # Replace the original value with the encrypted value
-#     $jsonObject.$field = $encryptedValue
-# }
-
-# # Convert the modified JSON object back to a JSON string
-# $encryptedJsonData = $jsonObject | ConvertTo-Json
-
-# # Write the encrypted JSON data back to the same JSON file
-# $encryptedJsonData | Set-Content -Path "your_output.json" -Force
-
-# # Output the encrypted JSON data
-# Write-Host "Encrypted JSON Data:"
-# Write-Host $encryptedJsonData
