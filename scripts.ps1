04cdf9c65b7129f8a3998f09d65bd43869f942e1
rajeshjanapati <rajeshjanapati@gmail.com>
2023-09-11 23:54:19 +0530

    66

--- scripts.ps1
+++ scripts.ps1
@@ -55,24 +55,27 @@ $AES.Mode = [System.Security.Cryptography.CipherMode]::CBC
 
 # Loop through the specified fields and encrypt their values
 foreach ($field in $fieldsToEncrypt) {
-    $plaintext = $appdetailget.credentials[0].$field
+    # Check if the credentials array exists and has at least one item
+    if ($appdetailget.credentials.Count -gt 0) {
+        $plaintext = $appdetailget.credentials[0].$field
 
-    # Convert plaintext to bytes (UTF-8 encoding)
-    $plaintextBytes = [System.Text.Encoding]::UTF8.GetBytes($plaintext)
+        # Convert plaintext to bytes (UTF-8 encoding)
+        $plaintextBytes = [System.Text.Encoding]::UTF8.GetBytes($plaintext)
 
-    # Generate a random initialization vector (IV)
-    $AES.GenerateIV()
-    $IVBase64 = [System.Convert]::ToBase64String($AES.IV)
+        # Generate a random initialization vector (IV)
+        $AES.GenerateIV()
+        $IVBase64 = [System.Convert]::ToBase64String($AES.IV)
 
-    # Encrypt the data
-    $encryptor = $AES.CreateEncryptor()
-    $encryptedBytes = $encryptor.TransformFinalBlock($plaintextBytes, 0, $plaintextBytes.Length)
-    $encryptedBase64 = [System.Convert]::ToBase64String($encryptedBytes)
+        # Encrypt the data
+        $encryptor = $AES.CreateEncryptor()
+        $encryptedBytes = $encryptor.TransformFinalBlock($plaintextBytes, 0, $plaintextBytes.Length)
+        $encryptedBase64 = [System.Convert]::ToBase64String($encryptedBytes)
 
-    # Store the encrypted value back in the JSON data
-    $appdetailget.credentials[0].$field = @{
-        "EncryptedValue" = $encryptedBase64
-        "IV" = $IVBase64
+        # Store the encrypted value back in the JSON data
+        $appdetailget.credentials[0].$field = @{
+            "EncryptedValue" = $encryptedBase64
+            "IV" = $IVBase64
+        }
     }
 }
 
@@ -81,6 +84,8 @@ $encryptedJsonData = $appdetailget | ConvertTo-Json
 
 # Display the modified JSON data
 Write-Host $encryptedJsonData
+
+
 
 # To save the modified JSON data to a file, uncomment and customize the following line
 # $encryptedJsonData | Set-Content -Path $outputFilePath
